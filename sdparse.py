2536cc059261ef2ab9139b3e174c72d894816f95
Mike Fowler <mfowler@csumb.edu>
2014-10-25 12:05:16 -0700

    Changed pokes list to hold classes
    Removed superfluous code as well. Going to start small and add in later
    rather than clutter up the workspace.

--- sdparse.py
+++ sdparse.py
@@ -9,17 +9,6 @@ class Poke():
 	def __init__(self, name):
 		self.name = name
 
-class Team():
-	poke = []
-
-class Player():
-	name = ""
-	team = []
-
-def loadPlayers():
-	''' Make this function to load up previous data '''
-	pass
-
 pokes = []
 name = raw_input( "Please enter the name of the player: " )
 pov = bool( raw_input( "Does Snagem have POV? True or False: " ) )
@@ -30,11 +19,11 @@ with open(filename, "r") as infile:
 	for line in infile:
 		#Looks for list of name's pokemon and stores them
 		if name + "'s" == line.split(' ')[0]:
-			pokes.append( line.split(' ')[2] )
-			pokes.append( line.split(' ')[4] )
-			pokes.append( line.split(' ')[6] )
-			pokes.append( line.split(' ')[8] )
-			pokes.append( line.split(' ')[10] )
-			pokes.append( line.split(' ')[12] )
+			pokes.append( Poke(line.split(' ')[2]) )
+			pokes.append( Poke(line.split(' ')[4]) )
+			pokes.append( Poke(line.split(' ')[6]) )
+			pokes.append( Poke(line.split(' ')[8]) )
+			pokes.append( Poke(line.split(' ')[10]) )
+			pokes.append( Poke(line.split(' ')[12]) )
 			#Removes \r\n from last poke
-			pokes[5] = pokes[5][:len(pokes[5])-2]
+			pokes[5].name = pokes[5].name[:len(pokes[5].name)-2]
