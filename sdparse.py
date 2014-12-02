8ad7a62a3502652d54905011ecb2eb4321520557
Mike Fowler <mfowler@csumb.edu>
2014-12-02 01:04:10 -0800

    Tracks moves
    Added a TODO. Moves need to be changed at some point before output.
    Tracking works as is though.

--- sdparse.py
+++ sdparse.py
@@ -1,13 +1,11 @@
 class Poke():
-	name = ""
-	hasNick = False
-	nick = ""
-	moves = []
-	item = ""
-	ability = ""
-
 	def __init__(self, name):
 		self.name = name
+		self.hasNick = False
+		self.nick = ""
+		self.moves = []
+		self.item = ""
+		self.ability = ""
 
 pokes = []
 name = raw_input( "Who do you want to track: " )
@@ -23,19 +21,20 @@ if pov:
 	sendMessage.append("Go!")
 	sendSplit = 1
 	attMessage.append("")
+	attSplit = 0
 else:
 	sendMessage.append(name)
 	sendMessage.append( "sent" )
 	sendMessage.append( "out" )
 	# Takes into account names with spaces
 	sendSplit = len(name.split(' ')) + len(sendMessage) - 1
-
-print "sendMessage =", sendMessage
-print "sendSplit =", sendSplit
-print "len(name).split =", len(name.split(' '))
+	attMessage.append("The")
+	attMessage.append("opposing")
+	attSplit = 2
 
 with open(filename, "r") as infile:
 	for line in infile:
+		#print line
 		# Looks for list of name's pokemon and stores them.
 		# Have to make the line you check is at least as long as
 		# the player's name. Then if the player has a name with
@@ -44,7 +43,6 @@ with open(filename, "r") as infile:
 		# If anyone types their name with a 's and it catches again
 		# we'll only pay attention to the first 6 instances so
 		# it won't matter.
-		match = False
 		if len(line.split(' ')) > len(name.split(' '))-1:
 			if name.split(' ')[len(name.split(' '))-1] + "'s" == \
 			line.split(' ')[len(name.split(' '))-1]:
@@ -62,7 +60,6 @@ with open(filename, "r") as infile:
 					Poke(line.split(' ')[len(name.split(' '))+11]) )
 				#Removes \r\n from last pokemon
 				pokes[5].name = pokes[5].name[:len(pokes[5].name)-2]
-				match = False
 		
 		# Find the current poke. If the pattern maches,
 		# it's a switch line. First check looks for the
@@ -76,9 +73,11 @@ with open(filename, "r") as infile:
 			# Go through each word looking for the name of a pokemon
 			for word in line.split(' '):
 				for e in pokes:
-					# Checks for names without nicknames
+					# Checks for names without nicknames but sets
+					# nick to it's own name for easier checks
+					# later on.
 					if e.name + "!\r\n" == word:
-						print "Switched to", e.name
+						e.nick = e.name
 						currentPoke = curr
 						break
 					# Names found like this mean there is a
@@ -97,4 +96,24 @@ with open(filename, "r") as infile:
 				curr = 0
 				indexOfName += 1
 
-		# Looks to see if the current pokemon uses a new move 
+		# Looks to see if the current pokemon uses a new move
+		#if ' '.join(attMessage) in line and \
+		#" used " in line:
+		# USE THE CURRENT POKE VARIABLE
+		if ' '.join(attMessage) in line and " used " in line:
+			if pokes[currentPoke].nick in line:
+				indexOfUsed = 0
+				for e in line.split(' '):
+					indexOfUsed += 1
+					if e == "used":
+						break
+				if ' '.join(line.split(' ')[indexOfUsed:]) not in pokes[currentPoke].moves:
+					newMove = line.split(' ')[indexOfUsed:]
+					newMove = ' '.join(newMove)
+					# TODO: remove the !\r\n from moves before
+					#       adding it to the text file. Maybe
+					#       have a "cleansing" function before
+					#		the output. Or fix the above if so
+					#		that it can find "Knock Off" instead
+					#		of "Knock Off!\r\n"
+					pokes[currentPoke].moves.append(newMove)
